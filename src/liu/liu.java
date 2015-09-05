9d71f9e6c70ab36215cff5e51c73237f370f0c7d
liukaifa <1763685997@qq.com>
2015-09-05 15:07:43 +0800

    项目提交

---
+++ src/liu/liu.java
@@ -0,0 +1,14 @@
+package liu;
+
+public class liu {
+
+	public liu() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/liu/liuTest.java
@@ -0,0 +1,19 @@
+package liu;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class liuTest {
+
+	@Test
+	public void testLiu() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
